5fbf64205bd302bf5192fb4f3e0344dd2aae75e9
ErvinChavez <ervinchavez31@gmail.com>
2026-03-22 19:10:14 -0400

    Initial commit - project setup

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+// pages/index.js
+
+export default function Home() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-50">
+      <h1 className="text-4xl font-bold text-green-600">
+        Chavez Tree Service Frontend
+      </h1>
+    </div>
+  );
+}
