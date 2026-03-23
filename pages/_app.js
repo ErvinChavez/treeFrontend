ef55f210c0c6268aacf53090113182e50d5daf6e
ErvinChavez <ervinchavez31@gmail.com>
2026-03-22 22:40:58 -0400

    made the connection from the frontend to the backend graphQL

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,15 @@
 // pages/_app.js
 import '../styles/globals.css'; // import Tailwind and any global CSS
+import { ApolloProvider } from '@apollo/client'; //the wrapper component
+import client from "@/lib/apollo"; //the bridge with apollo.js
+
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />; // renders the current page
+  return (
+    //Wrap the entire app in the ApolloProvider
+    //This causes the client connection to every single page and component in your project
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 }
