f31a4cdaf89809fa49218719beea9d128560d1d0
ErvinChavez <ervinchavez31@gmail.com>
2026-03-26 22:21:22 -0400

    start of creat jobs

---
+++ pages/admin/jobs.js
@@ -0,0 +1,80 @@
+import { useEffect } from "react";
+import  { useRouter } from  "next/router";
+import { gql } from "@apollo/client";
+import { useQuery } from "@apollo/client/react";
+import { isAuthenticated } from "@/utils/auth";
+
+//GraphQL query
+const GET_JOBS = gql`
+  query {
+    jobs {
+      id
+      status
+      createdAt
+
+      client {
+        name
+        email
+        phone
+      }
+
+      services {
+        id
+        name
+      }
+    }
+  }
+`;
+
+export default function Jobs() {
+    const router = useRouter();
+
+    //protect route
+    useEffect(() => {
+        if (!isAuthenticated()) {
+            router.push("/admin/login");
+        }
+    }, []);
+
+    const { data, loading, error } = useQuery(GET_JOBS);
+
+    if (loading) return <p className="p-6">Loading jobs...</p>;
+    if (error) return <p className="p-6">Error loading jobs</p>;
+
+    return (
+        <div className="p-6">
+          <h1 className="text-3xl font-bold mb-4">Jobs</h1>
+
+          <div className="grid gap-4">
+            {data.jobs.math((job) => (
+                <div key={job.id} className="border p-4 rounded shadow">
+
+                    <p className="font-semibold">Status: {job.status}</p>
+
+                    <div className="mt-2">
+                        <p><strong>Client:</strong> {job.client.name}</p>
+                        <p>{job.client.email}</p>
+                        <p>{job.client.phone}</p>
+                    </div>
+
+                    <div className="mt-2">
+                        <p className="font=semibold">Services:</p>
+                        <ul className="list-disc ml-5">
+                             {job.services.map((service)) => (
+                                <li key={service id}> {service.className}</li>
+                             )
+                            constructor(parameters) {
+                                
+                            }
+                        }>
+
+                        </ul>
+                    </div>
+
+                </div>
+            ))}
+
+          </div>
+        </div>
+    )
+}
