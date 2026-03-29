33f49866f4a8d623625deb3908f24f1ee2fe21ae
ErvinChavez <ervinchavez31@gmail.com>
2026-03-28 23:00:24 -0400

    added employees to jobs

--- pages/admin/jobs.js
+++ pages/admin/jobs.js
@@ -1,8 +1,9 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import  { useRouter } from  "next/router";
 import { gql } from "@apollo/client";
 import { useMutation, useQuery } from "@apollo/client/react";
 import { isAuthenticated } from "@/utils/auth";
+
 
 //GraphQL query
 const GET_JOBS = gql`
@@ -11,14 +12,16 @@ const GET_JOBS = gql`
       id
       status
       createdAt
-
       client {
         name
         email
         phone
       }
-
       services {
+        id
+        name
+      }
+      employees {
         id
         name
       }
@@ -26,13 +29,34 @@ const GET_JOBS = gql`
   }
 `;
 
-//Mutation
+//get all employees
+const GET_EMPLOYEES = gql`
+  query {
+    employees {
+      id
+      name
+    }
+  }
+`;
+
+//GraphQL Mutations
+
+//update job status
 const UPDATE_JOB_STATUS = gql`
   mutation UpdateJobStatus($jobId: Int!, $newStatus: String!) {
     updateJobStatus(jobId: $jobId, newStatus: $newStatus) {
       id
       status
     }
+  }
+`;
+
+//assign employees
+const ASSIGN_EMPLOYEES = gql`
+  mutation AssignEmployees($jobId: Int!, $employeeIds: [Int]) {
+    assignEmployeesToJob(jobId: $jobId, employeeIds: $employeeIds) {
+      id
+    }  
   }
 `;
 
@@ -50,13 +74,28 @@ export default function Jobs() {
         }
     }, []);
 
+    //Queries
     const { data, loading, error } = useQuery(GET_JOBS);
+    const { data: empData } = useQuery(GET_EMPLOYEES);
+
+    //local State
+    const [selectedEmployees, setSelectedEmployess] = useState({});
+    
+    //sync backend . local state
+    useEffect(()=> {
+      
+    })
+
+    //Mutations
     const [updateStatus] = useMutation(UPDATE_JOB_STATUS, {
       refetchQueries: [{ query: GET_JOBS}],
     });
 
+    const [assignEmployees] = useMutation(ASSIGN_EMPLOYEES, {
+      refetchQueries: [{query: GET_EMPLOYEES}],
+    })
 
-
+    //UI States
     if (!isAuthenticated || loading) return <p className="p-6">Loading jobs...</p>;
     if (error) return <p className="p-6">Error loading jobs</p>;
 
@@ -68,6 +107,7 @@ export default function Jobs() {
             {data.jobs.map((job) => (
                 <div key={job.id} className="border p-4 rounded shadow">
 
+                  //Status Dropdown
                     <div className="mt-2">
                       <strong>Status:</strong>
 
@@ -92,13 +132,15 @@ export default function Jobs() {
                         <option value="cancelled">Cancelled</option>
                       </select>
                     </div>
-
+                    
+                  //Client Info
                     <div className="mt-2">
-                        <p><strong>Client:</strong> {job.client.name}</p>
-                        <p>{job.client.email}</p>
-                        <p>{job.client.phone}</p>
+                        <p><strong>Client:</strong> {job.client?.name}</p>
+                        <p>{job.client?.email}</p>
+                        <p>{job.client?.phone}</p>
                     </div>
 
+                  //Services
                     <div className="mt-2">
                         <p className="font-semibold">Services:</p>
                         <ul className="list-disc ml-5">
@@ -107,6 +149,50 @@ export default function Jobs() {
                              ))}
                         </ul>
                     </div>
+
+                    //Assign Employees
+                    <div className="mt-3">
+                      <p className="font-semibond">Assign Employees:</p>
+
+                      {empData?.employees.map((emp) => {
+                        const isAssigned = job.employees.some(
+                          (e) => e.id === emp.id
+                        );
+
+                        return (
+                          <label key={emp.id} className="block">
+                            <input
+                              type="checkbox"
+                              checked={isAssigned}
+                              onChange={() => {
+                                let updated;
+
+                                if (isAssigned) {
+                                  updated = job.employees
+                                    .filter((e) => e.id !== emp.id)
+                                    .map((e) => Number(e.id));
+                                } else {
+                                  updated = [
+                                    ...job.employees.map((e) => Number(e.id)),
+                                    Number(emp.id),
+                                  ];
+                                }
+
+                                assignEmployees({
+                                  variables: {
+                                    jobId: Number(job.id),
+                                    employeeIds: updated,
+                                  },
+                                });
+                              }}
+                            />
+
+                            <span className="ml-2">{emp.name}</span>
+                          </label>
+                        );
+                      })}
+                    </div>
+
                 </div>
             ))}
 
