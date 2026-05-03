b452a3f68200cf257be304c8851470bacf4dd386
ErvinChavez <ervinchavez31@gmail.com>
2026-05-03 18:06:27 -0400

    fixed the call button business number

--- components/common/CallButton.js
+++ components/common/CallButton.js
@@ -1,7 +1,7 @@
 export default function CallButton() {
     return (
         <a 
-            href="tel:+4048861996" 
+            href="tel:+14048861996" 
             className="fixed bottom-0 left-0 w-full btn btn-accent rounded-none md:hidden z-50 shadow-lg"
         >
             Call Now  
