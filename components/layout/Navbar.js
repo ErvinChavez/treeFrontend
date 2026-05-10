233855c92a24ab0431290223a1705f6c8fac8709
ErvinChavez <ervinchavez31@gmail.com>
2026-05-09 23:59:36 -0400

    UI update on admin pages

--- components/layout/Navbar.js
+++ components/layout/Navbar.js
@@ -13,7 +13,7 @@ export default function NavBar() {
   return (
      <div className="bg-white border-b border-brand-wood-secondary/20">
 
-      <div className="w-full px-4 md:px-6 flex justify-between items-center py-3">
+      <div className="app-container flex justify-between items-center py-3">
       
         {/* Logo */}
         <div className="flex items-center gap-3">
