3990f8e16da3f214c49968152b6723baa1fcab85
ErvinChavez <ervinchavez31@gmail.com>
2026-06-07 17:18:18 -0400

    updated auth files

--- utils/auth.js
+++ utils/auth.js
@@ -8,6 +8,7 @@ export const setToken = (token) => {
 
 //get token from requests
 export const getToken = () => {
+    if (typeof window === "undefined") return null;
     return localStorage.getItem("token")
 };
 
@@ -21,20 +22,22 @@ export const isAuthenticated = () => {
   if (typeof window === "undefined") return false;
 
   const token = localStorage.getItem("token");
-  console.log("TOKEN:", token);
 
   if (!token) return false;
 
   try {
     const decoded = jwtDecode(token);
-    console.log("DECODED:", decoded);
 
     const valid = decoded.exp * 1000 > Date.now();
-    console.log("IS VALID:", valid);
 
-    return valid;
+    if (!valid) {
+      localStorage.removeItem("token");
+      return false;
+    }
+
+    return true;
   } catch (err) {
-    console.log("JWT ERROR:", err);
+    localStorage.removeItem("token");
     return false;
   }
 };
